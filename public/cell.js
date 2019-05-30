efd202d92135891283dc8989122b6313eb85d2b6
skrap17 <51079148+skrap17@users.noreply.github.com>
2019-05-30 10:39:26 +0300

    Update cell.js

--- public/cell.js
+++ public/cell.js
@@ -5,6 +5,7 @@ function cell(i, j){
   this.changeble = false;
   this.highlighted = false;
   this.col = 0;
+  this.st = 1;
   this.corrupted = false;
   this.sure = 255;
   this.main = 80;
@@ -27,6 +28,7 @@ function cell(i, j){
       fill(this.col, this.sure);
     if (this.corrupted)
       fill(255, 0, 0, this.sure);
+    strokeWeight(this.st);
     text(this.n, this.i * w + 0.0625 * w + xoff, this.j * w + 1.0625 * w + yoff, w, w);
   }
 }
