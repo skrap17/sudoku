5b5331219f6fad45ee13eb505a27e912d8b85ba8
skrap17 <51079148+skrap17@users.noreply.github.com>
2019-05-30 18:17:57 +0300

    Update button.js

--- public/button.js
+++ public/button.js
@@ -1,6 +1,6 @@
 function button(x, y, img, action) {
-  this.x = x + xoff;
-  this.y = y + yoff;
+  this.x = x;
+  this.y = y;
   this.img = img;
   this.pressed = false;
   this.action = action;
@@ -9,13 +9,13 @@ function button(x, y, img, action) {
     noStroke();
     fill(247, 247, 22, 140);
     if (this.pressed)
-      rect(this.x, this.y, w, w - 2);
+      rect(this.x + xoff, this.y + yoff, w, w - 2);
     imageMode(CORNER);
-    image(this.img, this.x, this.y, w, w - 2);
+    image(this.img, this.x + xoff, this.y + yoff, w, w - 2);
   }
   
   this.press = function(){
-    if (mouseX >= this.x && mouseY >= this.y && mouseX <= this.x + w && mouseY <= this.y + w){
+    if (mouseX >= this.x + xoff && mouseY >= this.y + yoff && mouseX <= this.x + xoff + w && mouseY <= this.y + yoff + w){
       this.pressed = true;
       this.action();
     }
@@ -23,5 +23,10 @@ function button(x, y, img, action) {
   
   this.unpress = function(){
     this.pressed = false;
+  }
+  
+  this.pos = function(x, y){
+    this.x = x;
+    this.y = y;
   }
 }
