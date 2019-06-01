ce1582eabc71665df7c681cbb185baca3085c939
skrap17 <51079148+skrap17@users.noreply.github.com>
2019-06-01 12:12:37 +0300

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -280,3 +280,50 @@ function back() {
     for (let y = 0; y < height; y += bg.height)
       image(bg, x, y);
 }
+
+function touchStarted() {
+  if (ingame) {
+    penbttn.press();
+    erbttn.press();
+    rebttn.press();
+    let i = int((mouseX - xoff) / w);
+    if (mouseY <= height - w - 1 - yoff && mouseY >= w + 1 + yoff && mouseX <= width - 1 - xoff && mouseX >= 1 + xoff && game) {
+      let j = int((mouseY - yoff) / w) - 1;
+      if (sudoku.cell(i, j).changeble) {
+        start = true;
+        sudoku.cell(curr[0], curr[1]).highlighted = false;
+        sudoku.unhight(curr[0], curr[1]);
+        sudoku.cell(curr[0], curr[1]).main = 80;
+        sudoku.high(i, j);
+        sudoku.cell(i, j).main = 140;
+        sudoku.lookAtAll(i, j);
+        curr = [i, j];
+      }
+    } else if (mouseY > height - w - 1 - yoff && mouseY <= height - yoff && mouseX <= width - 1 - xoff && mouseX >= 1 + xoff && game) {
+      nums[i].highlighted = true;
+      curnum = i;
+      if (start) {
+        sudoku.unhight(curr[0], curr[1]);
+        sudoku.set(curr[0], curr[1], nums[i].n);
+        sudoku.lookAtAll(curr[0], curr[1]);
+        sudoku.corruption();
+        if (sudoku.solved())
+          win();
+      }
+    }
+    redraw();
+  }
+}
+
+function touchEnded() {
+  if (ingame) {
+    penbttn.unpress();
+    erbttn.unpress();
+    rebttn.unpress();
+    if (game) {
+      sudoku.corruption();
+      nums[curnum].highlighted = false;
+    }
+    redraw();
+  }
+}
