3600511333b5865e5ef90f566ca2250f18d89d6c
skrap17 <51079148+skrap17@users.noreply.github.com>
2019-06-06 13:36:31 +0300

    Add files via upload

--- public/interface.js
+++ public/interface.js
@@ -1,50 +1,86 @@
- var ingame = false;
-    var K;
-    
-    document.body.onload = function(){
-      setTimeout(function() {
-        var pre = document.getElementById("pre");
-        var ld = document.getElementById("ld");
-        pre.style.display = "none";
-        ld.style.display = "none";
-        var x = document.getElementById("b1");
-        x.style.display = "inline-block";
-      }, 500);
-    }
-    
-    function startGame() {
-      var x = document.getElementById("b1");
-      x.style.display = "none";
-      var e = document.getElementById("e");
-      var m = document.getElementById("m");
-      var h = document.getElementById("h");
-      e.style.display = "inline-block";
-      m.style.display = "inline-block";
-      h.style.display = "inline-block";
-    }
+var K;
 
-    function butt() {
-      var e = document.getElementById("e");
-      var m = document.getElementById("m");
-      var h = document.getElementById("h");
-      e.style.display = "none";
-      m.style.display = "none";
-      h.style.display = "none";
-      ingame = true;
-      setup();
-    }
+document.body.onload = function() {
+  setTimeout(function() {
+    var ld = document.getElementById("ld");
+    ld.style.display = "none";
+    var x = document.getElementById("b1");
+    x.style.display = "inline-block";
+  }, 500);
+}
 
-    function easy() {
-      K = 10;
-      butt();
-    }
+function startGame() {
+  var x = document.getElementById("b1");
+  x.style.display = "none";
+  var e = document.getElementById("e");
+  var m = document.getElementById("m");
+  var h = document.getElementById("h");
+  e.style.display = "inline-block";
+  m.style.display = "inline-block";
+  h.style.display = "inline-block";
+}
 
-    function medium() {
-      K = 30;
-      butt();
-    }
+function butt() {
+  var pre = document.getElementById("pre");
+  pre.style.display = "none";
 
-    function hard() {
-      K = 50;
-      butt();
-    }
+  var e = document.getElementById("e");
+  var m = document.getElementById("m");
+  var h = document.getElementById("h");
+  e.style.display = "none";
+  m.style.display = "none";
+  h.style.display = "none";
+  newGame();
+  redraw();
+}
+
+function easy() {
+  K = 10;
+  butt();
+}
+
+function medium() {
+  K = 30;
+  butt();
+}
+
+function hard() {
+  K = 50;
+  butt();
+}
+
+function resume() {
+  let ovl = document.getElementById("pre");
+  ovl.style.display = "none";
+  let cont = document.getElementById("cont");
+  cont.style.display = "none";
+  let ng = document.getElementById("ng");
+  ng.style.display = "none";
+  let rest = document.getElementById("rest");
+  rest.style.display = "none";
+  start = true;
+}
+
+function NewGame() {
+  let cont = document.getElementById("cont");
+  cont.style.display = "none";
+  let ng = document.getElementById("ng");
+  ng.style.display = "none";
+  let rest = document.getElementById("rest");
+  rest.style.display = "none";
+
+  var e = document.getElementById("e");
+  var m = document.getElementById("m");
+  var h = document.getElementById("h");
+  e.style.display = "inline-block";
+  m.style.display = "inline-block";
+  h.style.display = "inline-block";
+
+}
+
+function restart() {
+   reset();
+   s = 0;
+   redraw();
+   resume(); 
+}
--- public/sketch.js
+++ public/sketch.js
@@ -9,10 +9,11 @@ let penimg;
 let bg, field;
 // let vert;
 // let horiz;
-let penbttn, erbttn, rebttn;
+let penbttn, erbttn, rebttn, pbttn;
 let xoff, yoff;
 let erimg;
 let reimg;
+let paimg;
 let s = 0;
 
 function preload() {
@@ -20,82 +21,82 @@ function preload() {
   field = loadImage("bg.png");
   erimg = loadImage("eraser.png");
   reimg = loadImage("restart.png");
+  paimg = loadImage("pause.png");
   bg = loadImage("cloudy-day.png");
   // vert = loadImage("vertical_line.png");
   // horiz = loadImage("gorizontal_line.png");
 }
 
 function setup() {
-  if (ingame) {
-    d = dims();
-    xoff = abs(windowWidth - d[0]) * 0.5;
-    yoff = abs(windowHeight - d[1]) * 0.5;
-    cnv = createCanvas(windowWidth, windowHeight);
-    cnv.style('display', 'block');
-    cnv.position(0, 0);
-    sudoku = new grid();
-    sudoku.fill();
+  d = dims();
+  xoff = abs(windowWidth - d[0]) * 0.5;
+  yoff = abs(windowHeight - d[1]) * 0.5;
+  cnv = createCanvas(windowWidth, windowHeight);
+  cnv.position(0, 0);
+  sudoku = new grid();
+  sudoku.fill();
 
-    for (let i = 0; i < 9; i++) {
-      nums.push(new cell(i, 9));
-      nums[i].n = i + 1;
-      nums[i].changeble = true;
-      nums[i].col = color(204, 56, 52);
-      nums[i].st = 3;
-    }
-
-    penbttn = new button(7 * w, 0, penimg, pencil);
-    erbttn = new button(8 * w, 0, erimg, erase);
-    rebttn = new button(6 * w, 0, reimg, reset);
-
-    setInterval(timeIt, 1000);
-
-    noLoop();
-    redraw();
+  for (let i = 0; i < 9; i++) {
+    nums.push(new cell(i, 9));
+    nums[i].n = i + 1;
+    nums[i].changeble = true;
+    nums[i].col = color(204, 56, 52);
+    nums[i].st = 3;
   }
+
+  penbttn = new button(7 * w, 0, penimg, pencil);
+  erbttn = new button(8 * w, 0, erimg, erase);
+  rebttn = new button(6 * w, 0, reimg, reset);
+  pbttn = new button(0, 0, paimg, pause);
+
+  setInterval(timeIt, 1000);
+
+  noLoop();
 }
 
 function draw() {
-  if (ingame) {
-    back();
-    imageMode(CENTER);
-    //image(bg, width / 2, height / 2);
-    image(field, width / 2, height / 2, (width - 2 * xoff), (height - 2 * yoff) - 2 * w);
-    showTime()
-    penbttn.show();
-    erbttn.show();
-    rebttn.show();
-    for (let i in nums)
-      nums[i].show();
-    sudoku.show();
-  }
+  back();
+  imageMode(CENTER);
+  //image(bg, width / 2, height / 2);
+  image(field, width / 2, height / 2, (width - 2 * xoff), (height - 2 * yoff) - 2 * w);
+  showTime()
+  penbttn.show();
+  erbttn.show();
+  rebttn.show();
+  pbttn.show();
+  for (let i in nums)
+    nums[i].show();
+  sudoku.show();
 }
 
 function mousePressed() {
-  if (ingame) {
+  if (start) {
     penbttn.press();
     erbttn.press();
     rebttn.press();
+    pbttn.press();
     let i = int((mouseX - xoff) / w);
     if (mouseY <= height - w - 1 - yoff && mouseY >= w + 1 + yoff && mouseX <= width - 1 - xoff && mouseX >= 1 + xoff && game) {
       let j = int((mouseY - yoff) / w) - 1;
-      if (sudoku.cell(i, j).changeble) {
-        start = true;
-        sudoku.cell(curr[0], curr[1]).highlighted = false;
-        sudoku.unhight(curr[0], curr[1]);
-        sudoku.cell(curr[0], curr[1]).main = 80;
-        sudoku.high(i, j);
-        sudoku.cell(i, j).main = 140;
+      sudoku.cell(curr[0], curr[1]).highlighted = false;
+      if (sudoku.cell(curr[0], curr[1]).changeble)
+        sudoku.anti_rsc(curr[0], curr[1]);
+      sudoku.unhight(curr[0], curr[1]);
+      if (!sudoku.cell(i, j).changeble)
         sudoku.lookAtAll(i, j);
-        curr = [i, j];
-      }
+      else
+        sudoku.rsc(i, j);
+      sudoku.cell(curr[0], curr[1]).main = 80;
+      sudoku.cell(i, j).main = 140;
+      sudoku.cell(i, j).highlighted = true;
+      curr = [i, j];
     } else if (mouseY > height - w - 1 - yoff && mouseY <= height - yoff && mouseX <= width - 1 - xoff && mouseX >= 1 + xoff && game) {
       nums[i].highlighted = true;
       curnum = i;
-      if (start) {
-        sudoku.unhight(curr[0], curr[1]);
+      if (start && sudoku.cell(curr[0], curr[1]).changeble) {
+        //sudoku.unhight(curr[0], curr[1]);
         sudoku.set(curr[0], curr[1], nums[i].n);
-        sudoku.lookAtAll(curr[0], curr[1]);
+        //sudoku.lookAtAll(curr[0], curr[1]);
         sudoku.corruption();
         if (sudoku.solved())
           win();
@@ -106,65 +107,57 @@ function mousePressed() {
 }
 
 function mouseReleased() {
-  if (ingame) {
-    penbttn.unpress();
-    erbttn.unpress();
-    rebttn.unpress();
-    if (game) {
-      sudoku.corruption();
-    }
-    nums[curnum].highlighted = false;
-    redraw();
+  penbttn.unpress();
+  erbttn.unpress();
+  rebttn.unpress();
+  pbttn.unpress();
+  if (game) {
+    sudoku.corruption();
   }
+  nums[curnum].highlighted = false;
+  redraw();
 }
 
 function doubleClicked() {
-  if (ingame) {
-    if (game) {
-      let i = int(mouseX / w);
-      let j = int(mouseY / w) - 1;
-      if (i == curr[0] && j == curr[1])
-        sudoku.set(i, j, undefined);
-    }
-    redraw();
+  if (game) {
+    let i = int(mouseX / w);
+    let j = int(mouseY / w) - 1;
+    if (i == curr[0] && j == curr[1])
+      sudoku.set(i, j, undefined);
   }
+  redraw();
 }
 
 function keyPressed() {
-  if (ingame) {
-    if (key == ' ') {
-      sudoku.fill();
-      start = false;
-      game = true;
-      curr = [0, 0];
-    } else if (game) {
-      sudoku.unhight(curr[0], curr[1]);
-      sudoku.cell(curr[0], curr[1]).main = 80;
-      if (key == '1')
-        sudoku.set(curr[0], curr[1], 1);
-      else if (key == '2')
-        sudoku.set(curr[0], curr[1], 2);
-      else if (key == '3')
-        sudoku.set(curr[0], curr[1], 3);
-      else if (key == '4')
-        sudoku.set(curr[0], curr[1], 4);
-      else if (key == '5')
-        sudoku.set(curr[0], curr[1], 5);
-      else if (key == '6')
-        sudoku.set(curr[0], curr[1], 6);
-      else if (key == '7')
-        sudoku.set(curr[0], curr[1], 7);
-      else if (key == '8')
-        sudoku.set(curr[0], curr[1], 8);
-      else if (key == '9')
-        sudoku.set(curr[0], curr[1], 9);
-      sudoku.corruption();
-      sudoku.lookAtAll(curr[0], curr[1]);
-      if (sudoku.solved())
-        win();
-    }
-    redraw();
+  if (key == ' ') {
+    newGame();
+    //start = true;
+  } else if (game && sudoku.cell(curr[0], curr[1]).changeble) {
+    sudoku.cell(curr[0], curr[1]).main = 140;
+    if (key == '1')
+      sudoku.set(curr[0], curr[1], 1);
+    else if (key == '2')
+      sudoku.set(curr[0], curr[1], 2);
+    else if (key == '3')
+      sudoku.set(curr[0], curr[1], 3);
+    else if (key == '4')
+      sudoku.set(curr[0], curr[1], 4);
+    else if (key == '5')
+      sudoku.set(curr[0], curr[1], 5);
+    else if (key == '6')
+      sudoku.set(curr[0], curr[1], 6);
+    else if (key == '7')
+      sudoku.set(curr[0], curr[1], 7);
+    else if (key == '8')
+      sudoku.set(curr[0], curr[1], 8);
+    else if (key == '9')
+      sudoku.set(curr[0], curr[1], 9);
+    sudoku.corruption();
+    //sudoku.lookAtAll(curr[0], curr[1]);
+    if (sudoku.solved())
+      win();
   }
+  redraw();
 }
 
 
@@ -239,16 +232,17 @@ function pencil() {
 }
 
 function erase() {
-  if (start) {
-    sudoku.unhight(curr[0], curr[1]);
+  if (start && sudoku.cell(curr[0], curr[1]).changeble) {
     sudoku.set(curr[0], curr[1], undefined);
+    //sudoku.anti_rsc(curr[0], curr[1]);
     sudoku.cell(curr[0], curr[1]).sure = 255;
+    sudoku.cell(curr[0], curr[1]).highlighted = true;
   }
 }
 
 function reset() {
-  if (start) {
-    sudoku.unhight(curr[0], curr[1]);
+  if (start || true) {
+    sudoku.anti_rsc(curr[0], curr[1]);
     for (let i = 0; i < sudoku.cells.length; i++)
       if (sudoku.cells[i].changeble) {
         sudoku.cells[i].n = undefined;
@@ -258,7 +252,7 @@ function reset() {
 }
 
 function timeIt() {
-  if (ingame && start) {
+  if (start) {
     s++;
     redraw();
   }
@@ -271,12 +265,38 @@ function showTime() {
   textAlign(CENTER, CENTER);
   fill(0);
   let st = "";
-  st = str(numeral(int(s / 3600)).format('00')) + " : " + str(numeral(int(s / 60)).format('00')) + " : " + str(numeral(s %  60).format('00'));
-  text(st, xoff, yoff, 3 * w, w);
+  st = str(numeral(int(s / 3600)).format('00')) + " : " + str(numeral(int(s / 60)).format('00')) + " : " + str(numeral(s % 60).format('00'));
+  text(st, xoff + w, yoff, 3 * w, w);
 }
 
 function back() {
   for (let x = 0; x < width; x += bg.width)
     for (let y = 0; y < height; y += bg.height)
       image(bg, x, y);
+}
+
+function newGame() {
+  sudoku.fill();
+  game = true;
+  s = 0;
+  curnum = 0;
+  curr = [0, 0];
+  start = true;
+}
+
+function pause() {
+  start = false;
+  let ovl = document.getElementById("pre");
+  ovl.style.display = "inline-block";
+
+  let cont = document.getElementById("cont");
+  cont.style.display = "inline-block";
+
+  let ng = document.getElementById("ng");
+  ng.style.display = "inline-block";
+
+  let rest = document.getElementById("rest");
+  rest.style.display = "inline-block";
+
+
 }
